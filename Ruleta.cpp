f9ee865a45879e152b755f8e9a65ef8ef942fd96
PlezySZN <yomosantiago15@gmail.com>
2024-10-17 18:08:50 -0400

    se anadio la variable selectionNum

--- Ruleta.cpp
+++ Ruleta.cpp
@@ -3,6 +3,7 @@ using std::string, std::cout, std::cin;
 
     int main () {
         int randomNum;
+        int selectionNum;
         cout << "Hello, world!" << std::endl;
         return 0;  
     }
