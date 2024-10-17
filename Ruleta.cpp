594bb4981d4864c7fe70849a8b5854ebda371e36
PlezySZN <yomosantiago15@gmail.com>
2024-10-17 18:12:20 -0400

    Se elimino hello world

--- Ruleta.cpp
+++ Ruleta.cpp
@@ -5,6 +5,5 @@ using std::string, std::cout, std::cin;
         int randomNum;
         int selectionNum;
         cout << "Hello, world!" << std::endl;
-        cout << "Hello, world!" << std::endl;
         return 0;  
     }
