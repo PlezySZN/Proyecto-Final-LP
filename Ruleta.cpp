ecd2f25219b4a8323bbb769dc8378cfcec3ff075
KVNYDPLFR <yadielpau712@gmail.com>
2024-10-17 18:08:44 -0400

    otro hello world

--- Ruleta.cpp
+++ Ruleta.cpp
@@ -3,6 +3,8 @@ using std::string, std::cout, std::cin;
 
     int main () {
         int randomNum;
+        
+        cout << "Hello, world!" << std::endl;
         cout << "Hello, world!" << std::endl;
         return 0;  
     }
