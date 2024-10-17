6a76917209040f47a8424e6ad9b30ce8d00973eb
PlezySZN <yomosantiago15@gmail.com>
2024-10-17 18:06:39 -0400

    se eliminaron los comentarios de master

--- Ruleta.cpp
+++ Ruleta.cpp
@@ -4,9 +4,5 @@ using std::string, std::cout, std::cin;
     int main () {
         int randomNum;
         cout << "Hello, world!" << std::endl;
-        //================HARRYS===============
-        //================KEVEN===============
-      
         return 0;  
-        //================KEVEN===============
     }
